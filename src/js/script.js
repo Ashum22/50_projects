31cf0fc3d0a3049bbc15dbf307f5fa11a08a6edc
Oleksandr Shumylo <ashumulo@ukr.net>
2022-09-06 13:13:30 +0300

    Rotated Navigation added

--- src/js/script.js
+++ src/js/script.js
@@ -67,3 +67,17 @@ function update() {
 		next.disabled = false
 	}
 }
+
+// End of Progress Bar code
+
+// Start of Rotated Navigatoin code
+
+const open = document.getElementById('open')
+const close = document.getElementById('close')
+const container = document.querySelector('.container-rotated')
+
+open.addEventListener('click', () => container.classList.add
+('show-nav'))
+
+close.addEventListener('click', () => container.classList.remove
+('show-nav'))
